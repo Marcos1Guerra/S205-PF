027bc2b4427ef49d53293f3bdbb137c2cd92c417
Marcos1Guerra <marcos.guerra@ges.inatel.br>
2025-06-03 11:59:27 -0300

    crição /home e /matricula

--- matricula.js
+++ matricula.js
@@ -1,15 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("menu-toggle");
-  const menu = document.getElementById("menu");
+  const btnMenu = document.querySelectorAll(".menu-icon")[4];
+  const btnAvisos = document.querySelectorAll(".menu-icon")[0];
+  const conteudo2 = document.querySelector(".conteudo2");
+  const conteudo = document.querySelector(".conteudo");
 
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("show");
+  btnMenu.addEventListener("click", () => {
+    conteudo2.style.display = "block";
+    conteudo.style.display = "none";
+
+    setActiveButton(btnMenu);
   });
 
-  window.addEventListener("click", (e) => {
-    if (!menu.contains(e.target) && !toggle.contains(e.target)) {
-      menu.classList.remove("show");
-    }
+  btnAvisos.addEventListener("click", () => {
+    conteudo.style.display = "block";
+    conteudo2.style.display = "none";
+    
+    setActiveButton(btnAvisos);
   });
+
+  function setActiveButton(activeBtn) {
+    document.querySelectorAll(".menu-icon").forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    activeBtn.classList.add("active");
+  }
+  setActiveButton(btnAvisos);
 });
 
+function ajustaAltura() {
+  const alturaReal = window.innerHeight;
+  document.documentElement.style.setProperty(
+    "--altura-real",
+    `${alturaReal}px`
+  );
+}
+window.addEventListener("resize", ajustaAltura);
+window.addEventListener("DOMContentLoaded", ajustaAltura);
